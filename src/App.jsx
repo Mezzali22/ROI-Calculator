85132c8997e9fc7ece4ff81dd2fa85a66d3a400e
Steven Sun <yineng.sun0311@gmail.com>
2025-04-23 18:25:26 -0400

    Fix Tailwind CSS setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,10 @@
-// Placeholder content for src/App.jsx
+import React from "react";
+import ROICalculator from "./ROICalculator";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-white p-8">
+      <ROICalculator />
+    </div>
+  );
+}
