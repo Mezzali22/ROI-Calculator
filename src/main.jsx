242ad4df03501d82c18d300ebd14beaa6039ffd2
Steven Sun <yineng.sun0311@gmail.com>
2025-04-23 18:20:19 -0400

    Fix Tailwind CSS setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,1 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import ROICalculator from './ROICalculator.jsx';
-import './index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ROICalculator />
-  </React.StrictMode>
-);
+// Placeholder content for src/main.jsx
